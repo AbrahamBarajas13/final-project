24f26ed558d1a9219235618f37d09197827afd0a
AbrahamBarajas13 <101150898+AbrahamBarajas13@users.noreply.github.com>
2022-08-28 22:05:24 -0500

    first commit

---
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import StartButton from "../../Components/StartButton/StartButton";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <h1>I'm in homescreen.</h1>
+      <StartButton />
+    </div>
+  );
+};
+
+export default HomeScreen;
